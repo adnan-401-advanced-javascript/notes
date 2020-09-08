ce8713c3e2365c9697abbb45e340b77d335900b5
amuammer <amuammer@students.iugaza.edu.ps>
2020-09-08 16:37:40 +0300

    adding mongodb

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const mongoURL = process.env.MONGOOSE_URL;
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 const Note = require("./lib/model/notes");
 const Input = require("./lib/input");
 
 const input = new Input();
-if (input.isValid()) {
-  const action = input.getAction();
-  const payload = input.getPayload();
+const action = input.getAction();
+const payload = input.getPayload();
 
-  const ourNotes = new Note();
+const ourNotes = new Note();
 
-  ourNotes.execute(action, payload);
-}
+ourNotes.execute(action, payload);
